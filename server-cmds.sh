259896a89a0d494884571d8f1b56001cc13d8f31
Anouar Rfifa <anouarrafifa99@gmail.com>
2023-09-05 18:10:06 +0200

    updated server-cmds file so it gets the arg

--- server-cmds.sh
+++ server-cmds.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-export IMAGE=$1
-sudo docker compose -f docker-compose.yaml up -d
-echo "success
+export IMAGE="$1"
+sudo IMAGE="${IMAGE}" docker compose -f docker-compose.yaml up -d
+echo "success"
